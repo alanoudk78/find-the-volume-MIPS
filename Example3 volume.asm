6cf4e882b6f53575282bf4b3ef01256caf41bcc3
Alanoud Alzughaibi <108308950+alanoudk78@users.noreply.github.com>
2022-07-01 04:59:00 +0300

    Add files via upload

---
+++ Example3 volume.asm
@@ -0,0 +1,43 @@
+.data
+
+aside: .word 73
+
+bside: .word 14
+
+cside: .word 16
+
+volume: .word 0
+
+
+
+.text
+
+
+
+lw $t0,aside
+
+lw $t1,bside
+
+lw $t2,cside
+
+
+
+mul $t3,$t0,$t1
+
+mul $t4,$t3,$t2
+
+sw $t4,volume
+
+
+
+li $v0,1
+
+lw $a0,volume
+
+syscall
+
+
+
+li $v0,10
+
+syscall
